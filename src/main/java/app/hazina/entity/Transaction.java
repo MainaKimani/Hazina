6a08d201b28c608e942420b44b8d7c72ab87bd7f
Maina Kimani <kalexmaina@gmail.com>
2022-07-08 22:43:47 +0300

    Transaction file modified

--- src/main/java/app/hazina/entity/Transaction.java
+++ src/main/java/app/hazina/entity/Transaction.java
@@ -1,13 +1,24 @@
 package app.hazina.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.util.Date;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
+@Entity
+@Table(name = "transactions")
 
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String type;
     private Date date;
     private Integer accountNumber;
