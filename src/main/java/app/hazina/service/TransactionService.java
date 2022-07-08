be4d2bfc4c38adec52ad96b4277e791e1c989d03
Maina Kimani <kalexmaina@gmail.com>
2022-07-08 22:44:56 +0300

    Transaction service modified

--- src/main/java/app/hazina/service/TransactionService.java
+++ src/main/java/app/hazina/service/TransactionService.java
@@ -1,25 +1,22 @@
 package app.hazina.service;
 
 import app.hazina.entity.Transaction;
+import app.hazina.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class TransactionService {
-    public List<Transaction> findAllByAccountNumber(final Integer accountNumber) {
-        return List.of(
-                Transaction
-                        .builder()
-                        .type("credit")
-                        .date(new Date())
-                        .accountNumber(accountNumber)
-                        .currency("Ksh. ")
-                        .amount(1000.00)
-                        .merchantName("Equity")
-                        .merchantLogo(" ")
-                        .build()
-        );
+    @Autowired
+    public TransactionService(final TransactionRepository repository) {
+        this.repository = repository;
     }
+
+    public List<Transaction> findAllByAccountNumber (final Integer accountNumber) {
+        return repository.findAllByAccountNumber(accountNumber);
+    }
+
+    private final TransactionRepository repository;
 }
