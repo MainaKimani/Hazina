eea313b71a5f4b67ad59bd3ce76e0d535cbcd5b8
Maina Kimani <kalexmaina@gmail.com>
2022-07-08 22:41:48 +0300

    Transaction repository

---
+++ src/main/java/app/hazina/repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package app.hazina.repository;
+
+import app.hazina.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findAllByAccountNumber(final Integer accountNumber);
+}
