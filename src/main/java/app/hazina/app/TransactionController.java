96a92438a080c2fa06593b41806442dfab24604a
Maina Kimani <kalexmaina@gmail.com>
2022-07-08 22:42:45 +0300

    Transaction controller modified

--- src/main/java/app/hazina/app/TransactionController.java
+++ src/main/java/app/hazina/app/TransactionController.java
@@ -2,15 +2,17 @@ package app.hazina.app;
 
 import app.hazina.entity.Transaction;
 import app.hazina.service.TransactionService;
+import app.hazina.app.dto.TransactionDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import static java.util.stream.Collectors.toList;
 
 @RestController
-@RequestMapping("/api/v1/transactions")
+@RequestMapping("/api/transactions")
 public class TransactionController {
 
     public TransactionController(final TransactionService transactionService) {
@@ -18,8 +20,14 @@ public class TransactionController {
     }
 
     @GetMapping("/{accountNumber}")
-    public List<Transaction> findAllByAccountNumber(@PathVariable("accountNumber") final Integer accountNumber) {
-        return transactionService.findAllByAccountNumber(accountNumber);
+    public List<TransactionDto> findAllByAccountNumber(@PathVariable("accountNumber") final Integer accountNumber) {
+        return transactionService.findAllByAccountNumber(accountNumber)
+                .stream().map(this::map)
+                .collect(toList());
+    }
+
+    private TransactionDto map(final Transaction tr) {
+        return TransactionDto.apply(tr);
     }
 
     private final TransactionService transactionService;
