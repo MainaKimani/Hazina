869a45eade218b89b7f7a768caf8ed722e02768a
Maina Kimani <kalexmaina@gmail.com>
2022-07-09 06:07:35 +0300

    End-to-end testing

--- src/test/java/app/hazina/service/TransactionServiceTest.java
+++ src/test/java/app/hazina/service/TransactionServiceTest.java
@@ -1,52 +1,23 @@
 package app.hazina.service;
 
-import app.hazina.entity.Transaction;
-import app.hazina.repository.TransactionRepository;
+import app.hazina.HazinaApplication;
 import app.hazina.service.TransactionService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import java.util.Date;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
 
 
-class TransactionServiceTest {
+@SpringBootTest(classes = {HazinaApplication.class})
+public class TransactionServiceTest {
 
-    @Mock
-    private TransactionRepository transactionRepository;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+    @Autowired
+    private TransactionService transactionService;
 
     @Test
     public void testTransactionCount() {
-        when(transactionRepository.findAllByAccountNumber(anyInt()))
-                .thenReturn(transactions());
-
-        var transactionService = new TransactionService(transactionRepository);
-        assertEquals(1, transactionService.findAllByAccountNumber(1234567).size());
-    }
-
-    private List<Transaction> transactions() {
-        return List.of(
-                Transaction
-                        .builder()
-                        .type("credit")
-                        .date(new Date())
-                        .accountNumber(1234567)
-                        .currency("Ksh. ")
-                        .amount(1000.00)
-                        .merchantName("Equity")
-                        .merchantLogo("")
-                        .build()
-        );
+        assertEquals(2, transactionService.findAllByAccountNumber(12345678).size());
     }
 }
