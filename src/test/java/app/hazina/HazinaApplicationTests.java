1db2ca5870d32da9d06a46ba6050d153622de978
Maina Kimani <kalexmaina@gmail.com>
2022-07-09 06:07:06 +0300

    End-to-end testing

--- src/test/java/app/hazina/HazinaApplicationTests.java
+++ src/test/java/app/hazina/HazinaApplicationTests.java
@@ -1,14 +1,13 @@
 package app.hazina;
 
+import app.hazina.HazinaApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import app.hazina.HazinaApplication;
 
-@SpringBootTest
-class HazinaApplicationTests {
-
+@SpringBootTest(classes = {HazinaApplication.class})
+public class HazinaApplicationTests {
 	@Test
 	void contextLoads() {
-	}
 
+	}
 }
